d6b19607e9f1defca00ee9db278bf762ecc2eeda
abdelazizdiane94-hash <abdelaziz.diane94@gmail.com>
2025-10-14 17:55:44 +0200

    Add files via upload

---
+++ Mission 6/TP 9/calculatrice.js
@@ -0,0 +1,8 @@
+function somme()
+{
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+    var c = Number(a) + Number(b);
+    document.getElementById("resultat").value = c;
+
+}
